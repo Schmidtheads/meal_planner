c4a7d4245de79542f94e039e5e6ee5297c4c38f3
Mark Schmidt <mschmidt@esri.ca>
2020-10-29 22:56:16 -0400

    Start of cookbook app - added models.py, started views.py urls.py Added style.css and base.html to home app

---
+++ cookbook/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Cookbook(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publish_date = models.DateField()
+    edition = models.CharField(max_length=20)
+
+    
+    def __str__(self):
+        return f"'{self.title}' by {self.author}"
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
