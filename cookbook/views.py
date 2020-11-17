06a01f3b5f1e98cbd9e15d4c2e966b2ba3ba97b3
Mark Schmidt <mschmidt@esri.ca>
2020-11-16 21:24:36 -0500

    Added changes to details page to allow for updating the existing record.

--- cookbook/views.py
+++ cookbook/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.forms import modelform_factory
 
 from .models import Cookbook, Author
 
+CookbookForm = modelform_factory(Cookbook, exclude=[])
+
 def detail(request, id):
     cookbook = get_object_or_404(Cookbook, pk=id)
-    return render(request, "cookbook/detail.html", {"cookbook": cookbook})
+
+    if request.method == "POST":
+        form = CookbookForm(request.POST, instance=cookbook)
+        if form.is_valid():
+            form.save()
+            return redirect("cookbooks")
+    else:
+        form = CookbookForm(instance=cookbook)
+
+    return render(request, "cookbook/detail.html",
+                 {"form": form})
 
 
 def cookbooks(request):
+
     return render(request, "cookbook/list.html",
                   {"cookbooks": Cookbook.objects.all(),
                    "table_name": "cookbooks"})
