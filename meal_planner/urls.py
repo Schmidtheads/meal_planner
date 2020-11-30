08cddc64717391a7d9ebfe5e26d9ace4957dff10
Mark Schmidt <mschmidt@esri.ca>
2020-11-29 21:45:40 -0500

    Created stub page for meal calendar view.  Updated home app main page to put link to new meal stub and move location of cookbook list link.

--- meal_planner/urls.py
+++ meal_planner/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('cookbook/', include('cookbook.urls')),
     path('recipe/', include('recipe.urls')),
+    path('meal/', include('meal.urls')),
 ]
