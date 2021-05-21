9086aef68112ab0c501ab73f9c6f6cfdd0fcd406
Mark Schmidt <mrk_schmidt@yahoo.com>
2021-05-20 21:54:39 -0400

    Recipe Data Model update -moved Many-to-Many of Recipe types to Recipe class -re-created database migrations

--- recipe/models.py
+++ recipe/models.py
@@ -3,26 +3,24 @@ from cookbook.models import Cookbook
 
 # Create your models here.
 
+
+class RecipeType(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
 class Recipe(models.Model):
     name = models.CharField(max_length=50)
     cook_book = models.ForeignKey(Cookbook, on_delete=models.CASCADE)
     page_number = models.PositiveSmallIntegerField()
     notes = models.CharField(max_length=750)
+    recipe_types = models.ManyToManyField(RecipeType)
 
     def __str__(self):
         return f"{self.name} {self.cook_book}"
 
-
-class RecipeType(models.Model):
-    name = models.CharField(max_length=20)
-    recipes = models.ManyToManyField(Recipe)
-
-    def __str__(self):
-        return f"{self.name}"
-
-#likely rendered irrelevant by the ManyToManyField in RecipeType
-#class RecipetoType(models.Model):
-#    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
 
 class Diner(models.Model):
     first_name = models.CharField(max_length=25)
