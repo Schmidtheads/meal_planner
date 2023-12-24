3041818abe07062fca2ef5a4770d84b04eed49d8
Mark Schmidt <mrk.schmidt@gmail.com>
2023-12-23 23:28:38 -0500

    Updated sign-up/user creation form

--- home/forms.py
+++ home/forms.py
@@ -3,9 +3,10 @@ Definition of forms.
 """
 
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
-#from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
+
 
 class BootstrapAuthenticationForm(AuthenticationForm):
     """Authentication form which uses boostrap CSS."""
@@ -17,3 +18,13 @@ class BootstrapAuthenticationForm(AuthenticationForm):
                                widget=forms.PasswordInput({
                                    'class': 'form-control',
                                    'placeholder':'Password'}))
+
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
--- home/views.py
+++ home/views.py
@@ -5,9 +5,12 @@ Definition of views.
 from datetime import datetime
 from django.shortcuts import render, redirect
 from django.http import HttpRequest
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import Group
 
+from .forms import SignUpForm
+
+DEFAULT_USER_GROUP = 'Diner'
 
 def home(request):
     """Renders the home page."""
@@ -55,16 +58,39 @@ def about(request):
 
 
 def signup(request):
+    '''
+    Signing up a new user from this post
+    https://simpleisbetterthancomplex.com/tutorial/2017/02/18/how-to-create-user-sign-up-view.html
+    '''
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
+
+            # Automatically log user in
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
+
+            # Add user to default group
+            #default_group = Group.objects.get(name=DEFAULT_USER_GROUP) 
+            
+            default_group = Group.objects.filter(name=DEFAULT_USER_GROUP).first()
+            if default_group: 
+                default_group.user_set.add(user)  # type: ignore
+            else:
+                print(f'User group {DEFAULT_USER_GROUP} not found')
+            
             return redirect('home')
     else:
-        form = UserCreationForm()
-    return render(request, 'home/signup.html', {'form': form})
+        form = SignUpForm()
+    return render(request, 
+        'home/signup.html', 
+        {
+            'year': datetime.now().year,
+            'company': 'Schmidtheads Inc.',
+            'form': form
+        }
+    )
    
