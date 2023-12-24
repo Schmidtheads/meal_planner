f9b0869270e2cfc8ce556d32eee00bda23c9a221
Mark Schmidt <mrk_schmidt@yahoo.com>
2023-12-24 15:26:07 -0500

    Feature create account (#36)
    * Added view, template for creating new user
    
    * Updated sign-up/user creation form
    
    ---------
    
    Co-authored-by: Mark Schmidt <mrk.schmidt@gmail.com>

--- home/views.py
+++ home/views.py
@@ -3,8 +3,14 @@ Definition of views.
 """
 
 from datetime import datetime
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import Group
+
+from .forms import SignUpForm
+
+DEFAULT_USER_GROUP = 'Diner'
 
 def home(request):
     """Renders the home page."""
@@ -20,6 +26,7 @@ def home(request):
         }
     )
 
+
 def contact(request):
     """Renders the contact page."""
     assert isinstance(request, HttpRequest)
@@ -34,6 +41,7 @@ def contact(request):
         }
     )
 
+
 def about(request):
     """Renders the about page."""
     assert isinstance(request, HttpRequest)
@@ -47,3 +55,42 @@ def about(request):
             'year':datetime.now().year,
         }
     )
+
+
+def signup(request):
+    '''
+    Signing up a new user from this post
+    https://simpleisbetterthancomplex.com/tutorial/2017/02/18/how-to-create-user-sign-up-view.html
+    '''
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            # Automatically log user in
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+
+            # Add user to default group
+            #default_group = Group.objects.get(name=DEFAULT_USER_GROUP) 
+            
+            default_group = Group.objects.filter(name=DEFAULT_USER_GROUP).first()
+            if default_group: 
+                default_group.user_set.add(user)  # type: ignore
+            else:
+                print(f'User group {DEFAULT_USER_GROUP} not found')
+            
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 
+        'home/signup.html', 
+        {
+            'year': datetime.now().year,
+            'company': 'Schmidtheads Inc.',
+            'form': form
+        }
+    )
+   
--- meal_planner/urls.py
+++ meal_planner/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
+    path('signup/', views.signup, name='signup'),
     path('login/',
          LoginView.as_view
          (
